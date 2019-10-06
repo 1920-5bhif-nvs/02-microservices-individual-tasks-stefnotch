fd6ec9a425c46224d0dd538af5648f9dcb3e3ca6
Stefnotch <brandmairstefan@gmail.com>
2019-10-06 20:16:53 +0200

    Revert "Use krankenhaus as a base project"
    This reverts commit ad5a5232605b39ec39866f6698868cd40e451918.

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# 02-microservices-individual-tasks-stefnotch
-
-Base taken from https://github.com/1920-5bhif-nvs/01-assignment-quarkus-jpa-stefnotch
+02-microservices-individual-tasks-stefnotch
--- src/main/java/at/htl/krankenhaus/rest/RestConfig.java
+++
@@ -1,8 +0,0 @@
-package at.htl.krankenhaus.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class RestConfig extends Application { //NOTE TO SELF: extends Application is important & easy to forget
-}
